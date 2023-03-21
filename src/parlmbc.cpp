e4d01af2ca6d6372a3851f7a579b779f0565b370
PunchShadow <littleuniverse24@gmail.com>
2023-03-21 22:01:35 +0800

    Initilize repo structure and design file reader

---
+++ src/parlmbc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <tbb/tbb.h>
+#include <stdio.h>
+#include "parlbmc.hpp"
+
+using namespace std;
+
+
+int parlmbc()
+{
+
+}
+
+
+int main()
+{
+
+}
---
+++ test/unit_test/file2graph/file2graph.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+
+
+#include "BiGraph.hpp"
+
+using namespace std;
+
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        cout << "Too few parameters!!!!" << endl;
+        return 0;
+    } 
+
+    string file_path = argv[1];
+    
+    cout << "file path is: " << file_path << endl;
+
+    BiGraph<int, int, int> BPG;
+    
+    BPG.read(file_path, 2, 0);
+
+    BPG.PrintSetSize();
+
+    return 0;
+}
